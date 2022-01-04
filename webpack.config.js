73aa393ab5a158a3642f88065e7f9505e6f216b7
r616on <r616on@gmail.com>
2022-01-04 13:26:16 +0300

    no_appvier

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,51 @@
-const path = require('path')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const HtmlWebpackPugPlugin = require('html-webpack-pug-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPugPlugin = require("html-webpack-pug-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
   entry: {
-    script: './js/_script.js'
+    script: "./src/js/_script.js",
   },
 
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'build')
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
   },
 
   plugins: [
     // Generating HTML
-    new HtmlWebpackPlugin({ template: 'pug/_index.pug', filename: 'index.html' }),
+    new HtmlWebpackPlugin({
+      template: "src/pug/_index.pug",
+      filename: "index.html",
+    }),
     new HtmlWebpackPugPlugin(),
 
-    new MiniCssExtractPlugin({ filename: 'style.css' }), // Generating CSS
+    new MiniCssExtractPlugin({ filename: "style.css" }), // Generating CSS
 
-    new CopyWebpackPlugin([{ from: 'img', to: 'img' }]), // Copy images
-    new CopyWebpackPlugin([{ from: 'comments.json', to: 'comments.json' }]) // Copy comments.json
+    new CopyWebpackPlugin([{ from: "img", to: "img" }]), // Copy images
+    new CopyWebpackPlugin([{ from: "comments.json", to: "comments.json" }]), // Copy comments.json
   ],
 
   optimization: {
     minimizer: [
       new OptimizeCSSAssetsPlugin({
         assetNameRegExp: /\.css$/g,
-        cssProcessor: require('cssnano'),
+        cssProcessor: require("cssnano"),
         cssProcessorPluginOptions: {
           preset: [
-            'default', {
-              discardComments: { removeAll: true }
-            }
-          ]
-        }
+            "default",
+            {
+              discardComments: { removeAll: true },
+            },
+          ],
+        },
       }),
 
-      new UglifyJsPlugin()
+      new UglifyJsPlugin(),
     ],
   },
 
@@ -51,7 +54,7 @@ module.exports = {
       // HTML
       {
         test: /\.pug$/,
-        loader: 'pug-loader'
+        loader: "pug-loader",
       },
 
       // CSS
@@ -63,59 +66,59 @@ module.exports = {
 
           // Regular CSS
           {
-            loader: require.resolve('css-loader'),
+            loader: require.resolve("css-loader"),
             options: {
               importLoaders: 1,
               sourceMap: true,
-              url: false
-            }
+              url: false,
+            },
           },
 
           // PostCSS with plugins
           {
-            loader: 'postcss-loader',
+            loader: "postcss-loader",
             options: {
               postcssOptions: {
                 plugins: [
-                  require('postcss-nested'),
-                  require('postcss-simple-vars')({
+                  require("postcss-nested"),
+                  require("postcss-simple-vars")({
                     variables: {
-                      $xxl: '1400px',
-                      $xxl_: '1399px',
-                      $xl: '1200px',
-                      $xl_: '1199px',
-                      $lg: '992px',
-                      $lg_: '991px',
-                      $md: '768px',
-                      $md_: '767px',
-                      $sm: '576px',
-                      $sm_: '575px'
-                    }
+                      $xxl: "1400px",
+                      $xxl_: "1399px",
+                      $xl: "1200px",
+                      $xl_: "1199px",
+                      $lg: "992px",
+                      $lg_: "991px",
+                      $md: "768px",
+                      $md_: "767px",
+                      $sm: "576px",
+                      $sm_: "575px",
+                    },
                   }),
-                  require('postcss-flexbugs-fixes'),
-                  require('postcss-preset-env')({
+                  require("postcss-flexbugs-fixes"),
+                  require("postcss-preset-env")({
                     autoprefixer: {
-                      flexbox: 'no-2009',
+                      flexbox: "no-2009",
                     },
                     stage: 3,
-                  })
+                  }),
                 ],
-                ident: 'postcss',
-                sourceMap: true
-              }
-            }
-          }
-        ]
-      }
-    ]
+                ident: "postcss",
+                sourceMap: true,
+              },
+            },
+          },
+        ],
+      },
+    ],
   },
 
   // Development server
   devServer: {
-    contentBase: path.join(__dirname, 'build'),
+    contentBase: path.join(__dirname, "dist"),
     port: 4000,
-    writeToDisk: true
+    writeToDisk: true,
   },
 
-  mode: process.env.NODE_ENV || 'development'
-}
+  mode: process.env.NODE_ENV || "development",
+};
